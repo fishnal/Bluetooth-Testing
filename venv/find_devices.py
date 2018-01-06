cb24834043d7c2fe994553ac3ab6ae5965fcf329
Vishal Patel <patelvishal3199@gmail.com>
2018-01-06 14:42:19 -0500

    Initial Commit

---
+++ venv/find_devices.py
@@ -0,0 +1,14 @@
+# finds nearby available bluetooth devices
+
+import bluetooth as bt
+
+print "Searching for nearby devices"
+
+nearby_devices = bt.discover_devices()
+
+for address in nearby_devices:
+    name = bt.lookup_name(address)
+    if name is None:
+        print address + " found"
+    else:
+        print address + " found: " + name
